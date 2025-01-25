496d114b7db7394e20d8c3c6d85db5cbf5905309
Paulo H Luiz P <54179987+pauloh-luizp@users.noreply.github.com>
2025-01-25 14:48:23 -0300

    Add files via upload

---
+++ fnGetMovieDetail/MovieResult.cs
@@ -0,0 +1,15 @@
+﻿namespace fnGetMovieDetail
+{
+    internal class MovieResult
+    {
+
+        public class Rootobject
+        {
+            public string id { get; set; }
+            public string title { get; set; }
+            public string year { get; set; }
+            public string video { get; set; }
+            public string thumb { get; set; }
+        }
+    }
+}
